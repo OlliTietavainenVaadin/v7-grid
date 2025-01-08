06ed5c404098e990609b0aed07d0c212b6d9e8e4
Olli Tietäväinen <ollit@vaadin.com>
2025-01-08 12:54:08 +0200

    custom component

--- src/main/java/org/vaadin/test/MyCustomComponent.java
+++ src/main/java/org/vaadin/test/MyCustomComponent.java
@@ -1,2 +1,16 @@
-package org.vaadin.test;public class MyCustomComponent {
+package org.vaadin.test;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+
+public class MyCustomComponent extends CustomComponent {
+
+    public MyCustomComponent() {
+        setCompositionRoot(new Label("Foo"));
+    }
+
+    public Component getRoot() {
+        return getCompositionRoot();
+    }
 }
